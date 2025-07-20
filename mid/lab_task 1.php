498e180f0e995d10d1891b0397bbf84123f65758
sharup <sharuppaul9@gmail.com>
2025-07-20 10:34:27 +0600

    Lab task -1

---
+++ mid/lab_task 1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <title> This is a Registation from </title>
+    <body>
+        <center>
+            <h1 style="color: blue;">AIUB  </h1>
+             <h3 style="color: red;">Registration From</h3>
+</center>
+<h3>Comeplete the Registation </h3>
+<table>
+    <tr>
+        <td>
+            Full name </td> <td> <input type="text"> </td>
+</tr>
+ <tr>
+
+        <td>
+            Email </td> <td> <input type="text"> </td>
+</tr>
+ <tr>
+        <td>
+            Password </td> <td> <input type="password"> </td>
+</tr>
+<tr>
+    <td>Gender:</td>
+    <td><input type="radio" name="des"> Male</td>
+     <td><input type="radio" name="des"> Other</td>
+</tr>
+<tr>
+    <td>Language known:</td>
+    <td>
+    <input type="checkbox"name="language"value="english">English
+     <input type="checkbox"name="language"value="bangla">Bangla
+      <input type="checkbox"name="language"value="hindi">Hindi
+</td>
+</tr>
+
+
+
+
+
+<tr>
+    <td> Country:</td>
+    <td>
+        <select name="country" id="country">
+        <option value="">--Select--</option>
+        <option value="bd">Bangladesh</option>
+        <option value="in">India</option>
+        <option value="usa">USA</option>
+</select>
+</td>
+</tr>
+<tr>
+    <td> Date of Birth:</td>
+    <td><input type="date" ></td>
+</tr>
+
+
+
+<tr>
+    <td> Upload photo:</td>
+    <td><input type="file" name="photo"></td>
+</tr>
+
+<tr>
+    <td>comment :</td>
+    <td><textarea></textarea></td>
+</tr>
+<tr>
+    <td><input type ="submit" name ="Register"></td>
+</tr>
+</table>
+</body>
+</html>
